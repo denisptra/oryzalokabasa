9e8f9360f64f4f667a7ecb0e5f50f28186d2b12c
denisptra <denitri0609@gmail.com>
2026-03-04 00:24:32 +0700

    Domain Backend

--- backend/Index.js
+++ backend/Index.js
@@ -4,5 +4,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 CMS Server running on http://localhost:${PORT}`);
+  console.log(`🚀 CMS Server running on https://oryzalokabasa.vercel.app/`);
 });
